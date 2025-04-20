8f6817d43beb5abe3b9cddefb45066f157490c6a
tuankien03 <nguyentuankien03@gmail.com>
2025-04-20 19:26:38 +0700

    fix db

--- Analytic-service/src/main/java/com/tuankien03/pastebin/entity/Analytic.java
+++ Analytic-service/src/main/java/com/tuankien03/pastebin/entity/Analytic.java
@@ -1,29 +1,32 @@
 package com.tuankien03.pastebin.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.Data;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "analytic", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"paste_id", "date_bucket"}, name = "pasteId_dateBucket_unique")
+@Table(name = "\"Analytics\"", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"pasteId", "dateBucket"}, name = "Analytics_pasteId_dateBucket_key")
 })
+@Data
 public class Analytic {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "date_bucket", nullable = false)
+    @Column(name = "\"dateBucket\"", nullable = false)
     private LocalDateTime dateBucket;
 
     @Column(nullable = false)
     private Integer views = 0;
 
-    @Column(name = "paste_id", nullable = false, insertable = false, updatable = false)
+    @Column(name = "\"pasteId\"", nullable = false)
     private Integer pasteId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "paste_id", referencedColumnName = "id")
+    @JoinColumn(name = "\"pasteId\"", referencedColumnName = "id", insertable = false, updatable = false)
+    @JsonBackReference
     private Paste paste;
-
 }
--- Analytic-service/src/main/java/com/tuankien03/pastebin/entity/Paste.java
+++ Analytic-service/src/main/java/com/tuankien03/pastebin/entity/Paste.java
@@ -1,15 +1,18 @@
 package com.tuankien03.pastebin.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table(name = "paste")
+@Table(name = "\"Paste\"", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"slug"}, name = "Paste_slug_key")
+})
 @Data
 public class Paste {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -20,15 +23,16 @@ public class Paste {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "\"createdAt\"", nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
 
-    @Column(name = "expiration_time")
+    @Column(name = "\"expirationTime\"")
     private LocalDateTime expirationTime;
 
-    @Column(name = "views_count", nullable = false)
+    @Column(name = "\"viewsCount\"", nullable = false)
     private Integer viewsCount = 0;
 
     @OneToMany(mappedBy = "paste", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
     private List<Analytic> analytics;
 }
