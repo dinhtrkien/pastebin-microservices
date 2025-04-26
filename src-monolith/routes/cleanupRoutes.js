2cd722764fa69178a9cab1e123a3cf6080a6a92c
dinhtrkien <21020636@vnu.edu.vn>
2025-04-26 10:27:44 +0700

    delete src-monolith/

--- src-monolith/routes/cleanupRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cleanupController = require('../domain/cleanup/cleanupController.js');
-
-// Endpoint to manually trigger cleanup
-router.get('/cleanup', cleanupController.runCleanup);
-
-module.exports = router;
