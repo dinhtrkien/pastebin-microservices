e9f006c5645106993eeb0c88fc99e83db2098598
tuankien03 <nguyentuankien03@gmail.com>
2025-05-04 09:00:59 +0700

    config cors

--- analytic/src/main/java/com/tuankien03/analytic/controller/AnalyticController.java
+++ analytic/src/main/java/com/tuankien03/analytic/controller/AnalyticController.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/analytics")
 @RequiredArgsConstructor
